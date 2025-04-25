f57a39ac4840a98e50e9a4425ee4fdb2ba1631a6
nskhattori <t.hattori@nskint.co.jp>
2025-04-25 14:39:13 +0900

    テスト作成

--- temp.cpp
+++ temp.cpp
@@ -1,10 +1,11 @@
 #define VALUE_ALARM_TEMP (80.0F)
+#define VALUE_REREASE_ALARM_TEMP (77.0F)
 bool g_bl_alarm_flag = false;
 
 bool func_judge_temp_alarm( float temp ) {
-    if (temp >= VALUE_ALARM_TEMP) {
+    if ((temp >= VALUE_ALARM_TEMP) && (g_bl_alarm_flag == false)) {
         g_bl_alarm_flag = true;
-    } else {
+    } else if ((temp <= VALUE_REREASE_ALARM_TEMP) && (g_bl_alarm_flag == true)) {
         g_bl_alarm_flag = false;
     }
     return g_bl_alarm_flag;
